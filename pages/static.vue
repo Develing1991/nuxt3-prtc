5d202e9eb3977480ab17f44a54e31737d382b7e0
Develing1991 <completed0728@gmail.com>
2023-03-20 08:46:41 +0900

    rendering mode test

--- pages/static.vue
+++ pages/static.vue
@@ -1,6 +1,14 @@
+<script setup>
+const time = ref(new Date());
+setInterval(() => {
+  time.value = new Date();
+}, 1000);
+</script>
 <template>
   <div>
     <h1>Static</h1>
-    <h2>{{ new Date() }}</h2>
+    <ClientOnly>
+      <h2>{{ time }}</h2>
+    </ClientOnly>
   </div>
 </template>
