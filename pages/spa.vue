e8721447028bd49a7b8f24f28f081620a915b1dc
Develing1991 <completed0728@gmail.com>
2023-03-20 08:51:27 +0900

    hybrid render test2

--- pages/spa.vue
+++ pages/spa.vue
@@ -1,14 +1,8 @@
-<script setup>
-const time = ref(new Date());
-setInterval(() => {
-  time.value = new Date();
-}, 1000);
-</script>
 <template>
   <div>
     <h1>SPA</h1>
-    <ClientOnly>
-      <h2>{{ time }}</h2>
-    </ClientOnly>
+    <div>
+      {{ new Date() }}
+    </div>
   </div>
 </template>
